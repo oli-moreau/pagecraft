60bd80115019a1f505a6cfedd5cb14bb2fc72978
Olivier Moreau <moreau.olivier@outlook.com>
2023-06-28 20:37:36 -0400

    Refactored rows structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,68 +3,67 @@
     <NavBar :show-editor="showEditor" @toggle-editor="toggleEditor"/>
     <DraggableBlocks v-if="showEditor"/>
     <section class="main-content">
-      <TransitionGroup name="slide">
-      <template v-for="(row, rowIndex) in pageRows" :key="rowIndex">
-        <div
-            class="row"
-            :class="{'row-border': showEditor}"
-            :style="{'height': `${rowHeights[rowIndex]}vh`}"
-        >
-          <div class="row-height-bar" v-if="showEditor">
-            <input
-                type="text"
-                :value="rowHeights[rowIndex]"
-                @keydown="numbersOnlyInput"
-                @keydown.enter.prevent="updateRowHeight(rowIndex, $event.target.value)"
-            >
-          </div>
-          <TransitionGroup name="slide">
-            <template v-for="block in row" :key="block.id">
-              <component
-                  :is="blockMap[block.type].component"
-                  :blockId="block.id"
-                  :showEditor="showEditor"
-                  :size="block.size"
-                  @remove-block="removeBlockHandler(rowIndex, block.id)"
+      <TransitionGroup name="row-slide">
+        <template v-for="(row, rowIndex) in pageRows" :key="row.id">
+          <div
+              class="row"
+              :class="{'row-border': showEditor}"
+              :style="{'height': `${row.height}vh`}"
+          >
+            <div class="row-height-bar" v-if="showEditor">
+              <input
+                  type="text"
+                  :value="row.height"
+                  @keydown="numbersOnlyInput"
+                  @keydown.enter.prevent="updateRowHeight(row.id, $event.target.value)"
               >
-              </component>
-              <div
-                  class="block-gap"
-                  :class="{'gap-being-dragged-over': isGapBeingDraggedOver[block.id]}"
-                  :style="getBlockGapStyle(block.id)"
-                  @dragover.prevent="blockGapHover(block.id)"
-                  @dragleave="blockGapHoverLeave(block.id)"
-                  @drop="blockDroppedInGap"
-              >
-                <div class="block-gap-width-bar" v-if="showEditor">
-                  <input
-                      type="text"
-                      :value="blockGapWidth[block.id]"
-                      @keydown="numbersOnlyInput"
-                      @keydown.enter.prevent="updateBlockGapWidth(block.id, $event.target.value)"
-                  >
+            </div>
+            <TransitionGroup name="block-slide">
+              <template v-for="block in row.data" :key="block.id">
+                <component
+                    :is="blockMap[block.type].component"
+                    :blockId="block.id"
+                    :showEditor="showEditor"
+                    :size="block.size"
+                    @remove-block="removeBlockHandler(row.id, block.id)"
+                >
+                </component>
+                <div
+                    class="block-gap"
+                    :class="{'gap-being-dragged-over': isGapBeingDraggedOver[block.id]}"
+                    :style="getBlockGapStyle(block.id)"
+                    @dragover.prevent="blockGapHover(block.id)"
+                    @dragleave="blockGapHoverLeave(block.id)"
+                    @drop="blockDroppedInGap(rowIndex, $event)"
+                >
+                  <div class="block-gap-width-bar" v-if="showEditor">
+                    <input
+                        type="text"
+                        :value="blockGapWidth[block.id]"
+                        @keydown="numbersOnlyInput"
+                        @keydown.enter.prevent="updateBlockGapWidth(block.id, $event.target.value)"
+                    >
+                  </div>
                 </div>
-              </div>
-            </template>
-          </TransitionGroup>
-          <DropZone
-              :style="{'height': `${rowHeights[rowIndex]}vh`}"
-              v-if="showEditor && row.length !== 4"
-              :blockId="blockId"
-              @droppedBlock="droppedInRow($event, rowIndex)"
-          />
-        </div>
-        <div class="row-gap" :style="{'height': `${rowGapHeights[rowIndex]}vh`}">
-          <div class="row-gap-height-bar" v-if="showEditor">
-            <input
-                type="text"
-                :value="rowGapHeights[rowIndex]"
-                @keydown="numbersOnlyInput"
-                @keydown.enter.prevent="updateRowGapHeight(rowIndex, $event.target.value)"
-            >
+              </template>
+            </TransitionGroup>
+            <DropZone
+                v-if="showEditor && row.length !== 4"
+                :blockId="blockId"
+                @droppedBlock="droppedInRow($event, row.id)"
+            />
           </div>
-        </div>
-      </template>
+          <div class="row-gap" :style="{'height': `${rowGapHeights[rowIndex]}vh`}">
+            <div class="row-gap-height-bar" v-if="showEditor">
+              <input
+                  type="text"
+                  :value="rowGapHeights[rowIndex]"
+                  @keydown="numbersOnlyInput"
+                  @keydown.enter.prevent="updateRowGapHeight(rowIndex, $event.target.value)"
+              >
+            </div>
+          </div>
+        </template>
       </TransitionGroup>
       <!--      v-bind="blockMap[block.type].props"-->
     </section>
@@ -95,6 +94,7 @@ export default {
   data() {
     return {
       showEditor: true,
+      rowId: 0,
       blockId: 1,
       pageRows: [],
       rowHeights: [],
@@ -111,36 +111,47 @@ export default {
       this.showEditor = !this.showEditor
     },
     droppedInRow(droppedBlock, rowIndex) {
-      const targetRow = this.pageRows[rowIndex];
-      this.blockId += 1
-      if (targetRow.length < 4) {
-        targetRow.push(droppedBlock)
+      // const targetRow = this.pageRows[rowIndex];
+      const targetRow = this.pageRows.find(row => row.id === rowIndex)
+      this.blockId++
+      if (targetRow.data.length < 4) {
+        targetRow.data.push(droppedBlock)
         this.blockGapWidth[droppedBlock.id] = 5
       } else {
-        this.pageRows.push([droppedBlock])
+        console.log('keep after 4?')
+        // this.pageRows.push([droppedBlock])
       }
     },
     droppedInNewRow(droppedBlock) {
-      this.blockId += 1
-      this.pageRows.push([droppedBlock])
+      this.blockId++
+      this.rowId++
+      const newRow = {
+        id: this.rowId,
+        height: 50,
+        data: [droppedBlock]
+      }
+      this.pageRows.push(newRow)
+      // this.pageRows.push([droppedBlock])
       this.rowHeights.push(50)
       this.rowGapHeights.push(5)
       this.blockGapWidth[droppedBlock.id] = 5
     },
-    removeBlockHandler(row, blockId) {
-      const index = this.pageRows[row].findIndex(block => block.id == blockId)
-      if (index !== -1) {
-        this.pageRows[row].splice(index, 1);
+    removeBlockHandler(rowId, blockId) {
+      const targetRowIndex = this.pageRows.findIndex(row => row.id === rowId)
+      const targetBlockIndex = this.pageRows[targetRowIndex].data.findIndex(block => block.id == blockId)
+      if (targetBlockIndex !== -1) {
+        this.pageRows[targetRowIndex].data.splice(targetBlockIndex, 1)
         delete this.blockGapWidth[blockId]
       }
-      if (this.pageRows[row].length === 0) {
-        this.pageRows.splice(row, 1);
-        this.rowHeights.splice(row, 1)
-        this.rowGapHeights.splice(row, 1)
+      if (this.pageRows[targetRowIndex].data.length === 0) {
+        this.pageRows.splice(targetRowIndex, 1);
+        this.rowHeights.splice(targetRowIndex, 1)
+        this.rowGapHeights.splice(targetRowIndex, 1)
       }
     },
-    updateRowHeight(rowIndex, value) {
-      this.rowHeights[rowIndex] = value;
+    updateRowHeight(rowId, value) {
+      const targetRow = this.pageRows.find(row => row.id === rowId)
+      targetRow.height = value
       event.target.blur()
     },
     updateRowGapHeight(rowIndex, value) {
@@ -167,28 +178,36 @@ export default {
     blockGapHoverLeave(blockId) {
       this.isGapBeingDraggedOver[blockId] = false
     },
-    blockDroppedInGap(ev) {
-      const movedBlockId = JSON.parse(ev.dataTransfer.getData('blockBeingMovedId'))
+    //TODO might re-think the whole block-gap concept
+    //TODO Should be its own thing instead of being bound to a block
+    blockDroppedInGap(rowIndex, event) {
+      console.log('todo')
+      // const movedBlockId = JSON.parse(event.dataTransfer.getData('blockBeingMovedId'))
+      // const currentBlockIndex = this.pageRows[rowIndex].data.findIndex(block => block.id === movedBlockId)
+      //
+      // const targetPageRowsIndex = this.pageRows.findIndex(row => row.some(block => block.id === this.gapBeingDraggedOverId))
+      // const targetBlockIndex = this.pageRows[targetPageRowsIndex].findIndex(block => block.id === this.gapBeingDraggedOverId)
+      // console.log(targetPageRowsIndex)
 
-      const currentPageRowsIndex = this.pageRows.findIndex(row => row.some(block => block.id === movedBlockId))
-      const currentBlockIndex = this.pageRows[currentPageRowsIndex].findIndex(block => block.id === movedBlockId)
 
-      const targetPageRowsIndex = this.pageRows.findIndex(row => row.some(block => block.id === this.gapBeingDraggedOverId))
-      const targetBlockIndex = this.pageRows[targetPageRowsIndex].findIndex(block => block.id === this.gapBeingDraggedOverId)
-
-      const currentBlock = this.pageRows[currentPageRowsIndex][currentBlockIndex]
-      const targetBlock = this.pageRows[targetPageRowsIndex][targetBlockIndex]
-
-      if (currentPageRowsIndex === targetPageRowsIndex) {
-        this.pageRows[currentPageRowsIndex].splice(currentBlockIndex, 1, targetBlock);
-        this.pageRows[targetPageRowsIndex].splice(targetBlockIndex, 1, currentBlock);
-      } else {
-        this.pageRows[currentPageRowsIndex].splice(currentBlockIndex, 1)
-        this.pageRows[targetPageRowsIndex].splice(targetBlockIndex+1, 0, currentBlock)
-      }
-
-      this.isGapBeingDraggedOver[movedBlockId] = false
-      this.isGapBeingDraggedOver[targetBlock.id] = false
+      // const currentBlockIndex = this.pageRows[currentPageRowsIndex].findIndex(block => block.id === movedBlockId)
+      //
+      // const targetPageRowsIndex = this.pageRows.findIndex(row => row.some(block => block.id === this.gapBeingDraggedOverId))
+      // const targetBlockIndex = this.pageRows[targetPageRowsIndex].findIndex(block => block.id === this.gapBeingDraggedOverId)
+      //
+      // const currentBlock = this.pageRows[currentPageRowsIndex][currentBlockIndex]
+      // const targetBlock = this.pageRows[targetPageRowsIndex][targetBlockIndex]
+      //
+      // if (currentPageRowsIndex === targetPageRowsIndex) {
+      //   this.pageRows[currentPageRowsIndex].splice(currentBlockIndex, 1, targetBlock);
+      //   this.pageRows[targetPageRowsIndex].splice(targetBlockIndex, 1, currentBlock);
+      // } else {
+      //   this.pageRows[currentPageRowsIndex].splice(currentBlockIndex, 1)
+      //   this.pageRows[targetPageRowsIndex].splice(targetBlockIndex+1, 0, currentBlock)
+      // }
+      //
+      // this.isGapBeingDraggedOver[movedBlockId] = false
+      // this.isGapBeingDraggedOver[targetBlock.id] = false
     },
   },
   computed: {
@@ -217,19 +236,4 @@ export default {
 }
 </script>
 <style>
-.slide-move,
-.slide-enter-active,
-.slide-leave-active {
-  transition: all .3s cubic-bezier(0.55, 0, 0.1, 1);
-}
-
-.slide-enter-from,
-.slide-leave-to {
-  opacity: 0;
-  transform: translateX(-30px);
-}
-
-.slide-leave-active {
-  //position: absolute;
-}
 </style>
